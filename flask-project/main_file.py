87daea298f2e21e6811849a9bf3362c3ac86fc52
Annstasia <staseann@gmail.com>
2019-04-03 21:08:35 +0300

    + Презентация

--- flask-project/main_file.py
+++ flask-project/main_file.py
@@ -17,16 +17,14 @@ db = SQLAlchemy(app)
 
 
 class UserModel(db.Model):
+    # Класс, отвечающий за авторизацию
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(80), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<UserModel {} {}>'.format(
-            self.id, self.username, self.password_hash)
-
 
 class ProductModel(db.Model):
+    # Класс, отвечающий за товары
     id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(100), unique=False, nullable=False)
     category = db.Column(db.String(100), unique=False, nullable=False)
@@ -38,18 +36,21 @@ class ProductModel(db.Model):
 
 
 class BasketModel(db.Model):
+    # Класс, отвечающий за корзину
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, unique=False, nullable=False)
     user_id = db.Column(db.Integer, unique=False, nullable=False)
 
 
 class OrderModel(db.Model):
+    # Класс, отвечающий за заказы
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, unique=False, nullable=False)
     user_id = db.Column(db.Integer, unique=False, nullable=False)
 
 
 class LoginForm(FlaskForm):
+    # Форма авторизации
     username = StringField('Логин', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
@@ -57,6 +58,7 @@ class LoginForm(FlaskForm):
 
 
 class NewProductForm(FlaskForm):
+    # Форма добавления товара
     category = SelectField('Categories', choices=[
         ('phones', 'Телефоны и аксессуары'), ('computers', 'Компьютеры и оргтехника'),
         ('electronics', 'Электроника'), ('appliances', 'Бытовая техника'), ('clothes', 'Одежда'),
@@ -72,6 +74,7 @@ class NewProductForm(FlaskForm):
 
 
 class RedactionProductForm(FlaskForm):
+    # Форма редактирования товара
     category = SelectField('Categories', choices=[
         ('phones', 'Телефоны и аксессуары'), ('computers', 'Компьютеры и оргтехника'),
         ('electronics', 'Электроника'), ('appliances', 'Бытовая техника'), ('clothes', 'Одежда'),
@@ -86,9 +89,8 @@ class RedactionProductForm(FlaskForm):
     delete = BooleanField('Удалить товар')
     submit = SubmitField('Добавить')
 
-
+'''
 def find_asked_name(name, ask):
-    print(name)
     nameList = name.lover().split()
     askList = ask.lover().split()
     count = 0
@@ -96,22 +98,25 @@ def find_asked_name(name, ask):
         for j in askList:
             if i == j:
                 count += 1
-    return count
+    return count'''
 
 
 db.create_all()
 
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/categories', methods=['GET', 'POST'])
 def main_page():
     if request.method == 'GET':
         return render_template('main_page.html')
     elif request.method == 'POST' and request.form.get('ask', False):
+        # Переход на точный поиск
         return redirect('/all/' + request.form['ask'])
 
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_in():
+    # Регистрация
     form = LoginForm()
     form.username.errors = []
     form.submit.label.text = 'Зарегистрироваться'
@@ -119,13 +124,16 @@ def sign_in():
         user_name = form.username.data
         password = form.password.data
         try:
+            # Регистрация пользователя и добавление в сессию. В UserModel username уникален.
+            # Если имя пользователя существует - исключение
             user1 = UserModel(username=user_name, password_hash=generate_password_hash(password))
             db.session.add(user1)
             db.session.commit()
             session['username'] = user1.username
             session['user_id'] = user1.id
+            # При добавлении в сессию, пользователь перенаправляется в личный кабинет
             return redirect("/lka")
-        except Exception as e:
+        except Exception:
             form.username.errors = ['Данный пользователь уже существует']
             return render_template('authorization.html', name='Регистрация', form=form)
     return render_template('authorization.html', name='Регистрация', form=form, )
@@ -133,11 +141,14 @@ def sign_in():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Авторизация
     form = LoginForm()
     if form.validate_on_submit():
         user_name = form.username.data
         password = form.password.data
+        # Поиск пользователя с введенным именем. Т.к. имя - уникальное значение, будет найден 1 или 0 пользователей
         user = UserModel.query.filter_by(username=user_name).first()
+        # Проверка существования пользователя и соответствия пароля
         if user and check_password_hash(user.password_hash, password):
             session['username'] = user.username
             session['user_id'] = user.id
@@ -148,6 +159,7 @@ def login():
 
 @app.route('/logout')
 def logout():
+    # Выход
     session.pop('username',0)
     session.pop('user_id',0)
     return redirect('/login')
@@ -155,32 +167,34 @@ def logout():
 
 @app.route('/lka', methods=['POST', 'GET'])
 def lka():
+    # Личный кабинет
     if 'username' not in session:
         return redirect('/login')
+    # Список всех товаров в корзине
     basket_products = [ProductModel.query.filter_by(id=i.product_id).first()
                        for i in BasketModel.query.filter_by(user_id=session['user_id']).all()]
 
     if request.method == 'POST':
-        print(i for i in request.form)
         for i in basket_products:
-            print(i)
+            # При отображении товаров в корзине существует кнопка оплаты,
+            # с именем и значением при нажатии == id товара корзины
             if request.form.get(str(i.id), None) == str(i.id):
-                print(i.id)
                 return redirect('/pay/' + str(i.id))
-    if request.method == 'GET':
-
-        order_products = [ProductModel.query.filter_by(id=i.product_id).first()
-                         for i in OrderModel.query.filter_by(user_id=session['user_id']).all()]
-        return render_template('lka.html', basket_products=basket_products, order_products=order_products)
+    # Список всех заказов
+    order_products = [ProductModel.query.filter_by(id=i.product_id).first()
+                      for i in OrderModel.query.filter_by(user_id=session['user_id']).all()]
+    return render_template('lka.html', basket_products=basket_products, order_products=order_products)
 
 
 @app.route('/lka/new_product', methods=['GET', 'POST'])
 def new_product():
+    # Добавление товара. Только для администратора (admin; qwerty)
     form = NewProductForm()
     if 'username' not in session or session['username'] != 'admin':
         return '''<h1>Доступ к странице закрыт</h1>'''
     else:
         if form.validate_on_submit():
+            # Добавление товара в БД
             product = ProductModel(product_name=form.product_name.data, category=form.category.data,
                                    cost=form.cost.data, count=form.count.data,
                                    s_description=form.s_description.data,
@@ -190,12 +204,16 @@ def new_product():
             path = os.path.join('static\\image\\', str(product.id))
             os.mkdir(path)
             files = request.files.getlist("files")
+            # Добавление изображений. Если изображения не заданы, создается пустая папка
+            # Номер папки соответствует id товара
             for i in range(len(files)):
                 end = files[i].filename.split('.')[-1]
                 if end not in ['jpg', 'jpeg', 'png', 'bmp', 'raw', 'gif', 'psd', 'tiff']:
                     form.submit.errors = ['Неверный формат изображения']
                     return render_template('new_product.html', form=form)
                 if i == 0:
+                    # Первое изображение отображается в категориях и на странице товара,
+                    # остальные - только на странице товара
                     product.main_photo = '\\' + path + '\\0.' + end
                     db.session.commit()
                 files[i].save(path + '\\' + str(i) + '.' + end)
@@ -208,35 +226,46 @@ def category_product(category):
     products = ProductModel.query.filter_by(category=category).all()
     if request.method == 'POST':
         if request.form.get('cost_sort', None) == 'cost_sort':
+            # Сортировка по возрастанию цены
             products = sorted(products, key=lambda s: int(s.cost))
         if request.form.get('cost_sort_inv', None) == 'cost_sort_inv':
+            # Сортировка по убыванию цены
             products = sorted(products, key=lambda s: -int(s.cost))
         if request.form.get('ask', False):
+            # Точный поиск
             return redirect('/all/' + request.form['ask'])
     return render_template('category_product.html', products=products)
 
 
 @app.route('/all/<ask>', methods=['POST', 'GET'])
 def all_categories_ask(ask):
+    # Поиск по точному названию
     products = ProductModel.query.filter_by(product_name=ask).all()
     if request.method == 'POST':
         if request.form.get('cost_sort', None) == 'cost_sort':
+            # Сортировка по возрастанию цены
             products = sorted(products, key=lambda s: int(s.cost))
         if request.form.get('cost_sort_inv', None) == 'cost_sort_inv':
+            # Сортировка по убыванию цены
             products = sorted(products, key=lambda s: -int(s.cost))
-        elif request.form.get('ask', False):
+        if request.form.get('ask', False):
+            # Поиск по следующему названию
             return redirect('/all/' + request.form['ask'])
-    return render_template('category_product.html', products=products[:100])
+    return render_template('category_product.html', products=products)
 
 
 @app.route('/categories/<category>/<int:id>', methods=['POST', 'GET'])
 def product_page(category, id):
+    # Страница товара
     form = RedactionProductForm()
     product = ProductModel.query.filter_by(id=id).first()
     if not product:
+        # Проверка существования продукта с заданным id
         return '''<h1 style="background-color: rgb(255, 140, 140)">Товар не найден</h1>'''
+    # Проверка наличия товара в корзине
     in_basket = True if 'user_id' in session and BasketModel.query.filter_by(
             product_id=id, user_id=session['user_id']).first() else False
+    # Список путей до изображений товара
     images = ['\\static\\image\\' + str(id) + '\\' + i
               for i in os.listdir('static\\image\\' + str(id))]
     number = len(images)
@@ -244,20 +273,28 @@ def product_page(category, id):
         if request.form.get('submit_button', None) == 'append':
             if 'user_id' not in session:
                 return redirect('/login')
+            # Добавление товара в корзину авторизованного пользователя
             basket_product = BasketModel(product_id=id, user_id=session['user_id'])
             db.session.add(basket_product)
             db.session.commit()
-            return redirect('/lka')
+            in_basket = True
 
         elif request.form.get('submit_button', None) == 'pay':
+            # Оплата товара, если он находится в корзине
             return redirect('/pay/' + str(id))
-        if form.validate_on_submit() and form.validate():
+
+        elif request.form.get('submit_button', None) == 'delete':
+            basket = BasketModel.query.filter_by(user_id=session['user_id'], product_id=id).first()
+            db.session.delete(basket)
+            db.session.commit()
+            in_basket = False
+        elif form.validate_on_submit() and form.validate():
+            # Редактирование информации о товаре. Поля, которые не нужно изменять остаются пустыми
             if form.category.data:
                 product.category = form.category.data
             if form.product_name.data:
                 product.product_name = form.product_name.data
             if form.cost.data:
-                print(form.cost.data)
                 if not form.cost.data.isdigit():
                     form.cost.errors = ['Неверный формат числа']
                 product.cost = form.cost.data
@@ -280,12 +317,17 @@ def product_page(category, id):
                     Неверный формат изображения<h1>'''
                 files[i].save(path + '\\' + str(number + i) + '.' + end)
             if form.delete.data:
+                # Удаление товара
+                # Удаление товара из таблици товаров
                 product_model = ProductModel.query.filter_by(id=id).first()
                 db.session.delete(product_model)
+                # Удаление товара из корзины всех пользователей
                 for i in BasketModel.query.filter_by(product_id=id).all():
                     db.session.delete(i)
+                # Удаление товара из заказов всех пользователей
                 for i in OrderModel.query.filter_by(product_id=id).all():
                     db.session.delete(i)
+                # Удаление папки с изображениями товара
                 shutil.rmtree('static/image/' + str(id))
                 db.session.commit()
                 return redirect('/categories/' + category)
@@ -297,27 +339,25 @@ def product_page(category, id):
 @app.route('/pay/<int:product_id>')
 def pay(product_id):
     try:
+        # Оплата товара. Магическим образом для оплаты товара достаточно нажать на кнопку оплаты
         product = ProductModel.query.filter_by(id=product_id).first()
+        # Проверка наличия товара на складе
         if product.count == '0':
             return '''Товара нет на складе'''
+        # Уменьшение количества товара
         product.count = str(int(product.count) - 1)
+        # Перенос товара из корзины в заказы
         basket_product = BasketModel.query.filter_by(product_id=product_id,
                                                      user_id=session['user_id']).first()
         order_product = OrderModel(user_id=session['user_id'], product_id=product_id)
         db.session.add(order_product)
         db.session.delete(basket_product)
         db.session.commit()
+
         return redirect('/lka')
     except Exception as e:
         return e
 
-'''
-product = BasketModel.query.filter_by(product_id=3).first()
-db.session.delete(product)
-db.session.commit()'''
-
-
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
-
